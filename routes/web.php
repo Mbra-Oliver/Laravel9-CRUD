5ac7f09cae0b1134798c6c95fe2efbe6d6b3ccda
Mbra-Oliver <96344566+Mbra-Oliver@users.noreply.github.com>
2022-04-16 15:18:55 +0000

    Complete CRUD

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [UserController::class, 'index'])->name('home');
+
+Route::get('new', [UserController::class, 'create'])->name('user.create');
+Route::post('store', [UserController::class, 'store'])->name('user.store');
+Route::delete('/delete/{user}', [UserController::class, 'delete'])->name('user.delete');
+Route::get('edit/{user}', [UserController::class, 'edit'])->name('user.edit');
+Route::put('update/{user}', [UserController::class, 'update'])->name('user.update');
